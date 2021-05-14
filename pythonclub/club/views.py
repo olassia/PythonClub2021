68da4612defc0ccc9a8b2823ef18cdcb24c880fb
Nicholas Jimenez <nicholasd.jimenez@gmail.com>
2021-05-14 16:06:47 -0700

    Model corrections commit

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
+from .models import Meeting, MeetingMinute, Resource, Event
 
 # Create your views here.
 def index(request): 
     return render(request, 'club/index.html')
+
+def members(request):    
