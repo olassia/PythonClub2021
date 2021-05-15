e640fb7f006ea49e646580e0aa1a34338bb80886
Nicholas Jimenez <nicholasd.jimenez@gmail.com>
2021-05-14 20:27:08 -0700

    Second resource view commit

--- pythonclub/club/views.py
+++ pythonclub/club/views.py
@@ -5,4 +5,6 @@ from .models import Meeting, MeetingMinute, Resource, Event
 def index(request): 
     return render(request, 'club/index.html')
 
-   
+def resources(request):
+    resource_list=Resource.objects.all()
+    return render(request, 'club/resources.html', {'resource_list': resource_list})
